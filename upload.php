b25599d13827a05205c19fb8dd204ff8c376769d
makiuchi <makiuchi@azillion.co.jp>
2018-11-23 17:21:37 +0900

    SQLite読み取り

--- upload.php
+++ upload.php
@@ -6,7 +6,11 @@ require __DIR__ . '/vendor/autoload.php';
 # Imports the Google Cloud client library
 use Google\Cloud\Speech\SpeechClient;
 
-//print_r($_FILES); 
+read_db('select * from os');
+
+$longitude = $_POST['longitude'];
+$latitude = $_POST['latitude'];
+
 $size = $_FILES['audio_data']['size']; 
 $input = $_FILES['audio_data']['tmp_name'];
 $output = $_FILES['audio_data']['name'] . time() .".wav"; 
@@ -20,21 +24,60 @@ $speech = new SpeechClient([
 
 # wav -> flac
 $flac = "wave.flac";
-$command = "ffmpeg -i $output $flac";
+$command = "ffmpeg -y -i $output $flac";
 exec($command, $out, $res);
 if($res) {
 	echo "Error Command: $command, ";
 	return;
 }
 
-# The audio file's encoding and sample rate
 $options = [
     'encoding' => 'FLAC'
 ];
 
 $results = $speech->recognize(fopen($flac, 'r'), $options);
 
-foreach ($results as $result) {
-    echo 'Transcription: ' . $result->alternatives()[0]['transcript'] . PHP_EOL;
+$text = "";
+if( !empty($results) ) {
+   $text = $results[0]->alternatives()[0]['transcript'];
 }
+
+if( strpos($text, '公園') !== false){
+  echo $text;
+} else {
+  echo $text;
+}
+echo $text;
+
+function read_db($sql, array $params = null) {
+    try {
+        $pdo = new PDO('sqlite:testdb.sqlite3');
+        // (毎回if文を書く必要がなくなる)
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // デフォルトのフェッチモードを連想配列形式に設定 
+        // (毎回PDO::FETCH_ASSOCを指定する必要が無くなる)
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        
+        // $stmt = $pdo->prepare("SELECT * FROM os WHERE id = ?");
+        // $stmt->execute(['1']);
+        $stmt = $pdo->prepare("SELECT * FROM os");
+        $stmt->execute($params);
+        $r1 = $stmt->fetchAll();
+        // 結果を確認
+        write_log($r1[0]['name']);
+    } catch (Exception $e) {
+        write_log('エラー発生');
+        write_log($e->getMessage());
+    }    
+}
+
+/**
+ * write logs
+ */
+function write_log($text) {
+    $f = fopen("chatbot.log", "a");
+    @fwrite($f, date("Y/m/d H:i:s") . "\t" . $text."\n");
+    fclose($f);    
+}
+
 ?>
